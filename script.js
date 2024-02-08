7e689fa733fd48937846227f962b89b219586db8
BG00000 <abakergabb@gmail.com>
2024-02-08 15:03:47 +1100

    commit

--- script.js
+++ script.js
@@ -1,69 +1,91 @@
-let myLibrary = []
+const addBtn = document.getElementById('add-btn')
+const bookDetails = document.getElementById('book-details')
 
-const bookSection = document.querySelector('#book-section')
-const blurOverlay = document.querySelector('#blur-overlay')
-const bookInfo = document.querySelector('#book-info')
+const detailForm = document.getElementById('book-details')
 
-const titleInput = document.querySelector('#title-input')
-const authorInput = document.querySelector('#author-input')
-const pagesInput = document.querySelector('#pages-input')
+const cancelBtn = document.getElementById('cancel-btn')
+const submitBtn = document.getElementById('submit-btn')
+const delBtn = document.getElementById('del-btn')
 
-const addBtn = document.querySelector('#add-btn')   
-const submitBtn = document.querySelector('#submit-btn')
+const bookTable = document.getElementById('book-table')
+
+const myLibrary = []
+
 
 addBtn.addEventListener('click', () => {
-    blurOverlay.style.webkitFilter = "blur(15px)"
-    bookInfo.style.visibility = 'visible'
-    bookInfo.style.opacity = '1'
+    bookDetails.style.display = "grid"
 })
 
-function Book(_title, _author, _pages, _read) {
-    this._title = _title
-    this._author = _author
-    this._pages = _pages
-    this._read = _read
+function createBook(title, author, pages, read, index) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.index = index
 }
 
-submitBtn.addEventListener('click', (event) => {
-    event.preventDefault()
-
-    blurOverlay.style.webkitFilter = "none"
-    bookInfo.style.visibility = 'hidden'
-    bookInfo.style.opacity = '0'
-
-    let readValue
-
-    if(document.querySelector('#yes-read').checked) {
-        readValue = true
+function submitFunc() {
+    let inRead = false
+    let inBookName = document.getElementById('in-book-name').value
+    let inBookAuthor = document.getElementById('in-book-author').value
+    let inBookPages = document.getElementById('in-book-pages').value
+    
+    if (document.querySelector('input[name="read"]:checked').value === 'Yes') {
+        inRead = "Yes"
     } else {
-        readValue = false
+        inRead = "No"
     }
 
-    let book = new Book(titleInput.value, authorInput.value, parseInt(pagesInput.value, 10), readValue)
+    bookDetails.style.display = 'none'
+
+    let book = new createBook(inBookName, inBookAuthor, inBookPages, inRead)
 
     myLibrary.push(book)
 
     console.log(myLibrary)
 
-    for(i = 0; i < myLibrary.length; i++) {
+    bookDetails.reset()
 
-        const div = document.createElement('div')
-        div.className = 'book-style'
-        bookSection.appendChild(div)
-    
-        const pTitle = document.createElement('p')
-        const pAuthor = document.createElement('p')
-        const pPages = document.createElement('p')
-        const pRead = document.createElement('p')
-    
-        pTitle.innerText = myLibrary[i]._title
-        pAuthor.innerText = myLibrary[i]._author
-        pPages.innerText = myLibrary[i]._pages
-        pRead.innerText = myLibrary[i]._read
-    
-        div.appendChild(pTitle)
-        div.appendChild(pAuthor)
-        div.appendChild(pPages)
-        div.appendChild(pRead)
-    }
+    createTable(inBookName, inBookAuthor, inBookPages, inRead)
+}
+
+function createTable(inBookName, inBookAuthor, inBookPages, inRead) {
+    const tableRow = document.createElement('tr')
+
+    const titleData = document.createElement('td')
+    const authorData = document.createElement('td')
+    const pagesData = document.createElement('td')
+    const readData = document.createElement('td')
+    const delCol = document.createElement('td')
+    const delBtn = document.createElement('button')
+
+    titleData.innerText = inBookName
+    authorData.innerText = inBookAuthor
+    pagesData.innerText = inBookPages
+    readData.innerText = inRead
+    delBtn.innerText = 'Delete'
+    delBtn.setAttribute('id', 'del-btn')
+
+    tableRow.appendChild(titleData)
+    tableRow.appendChild(authorData)
+    tableRow.appendChild(pagesData)
+    tableRow.appendChild(readData)
+    tableRow.appendChild(delCol)
+    delCol.appendChild(delBtn)
+
+    document.getElementById('book-table').appendChild(tableRow)
+}
+
+
+
+
+bookDetails.addEventListener('submit', (event) => {
+    event.preventDefault()
+    submitFunc()
 })
+
+cancelBtn.addEventListener('click', () => {
+    bookDetails.style.display = "none"
+})
+
+
